d8ec49d3ae827e7902fe6dc1c3347eb7b2e4e0ec
Tommy Parnell <tommy.parnell@himarley.com>
2022-06-15 15:28:44 -0400

    quad ot

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ fastify.get('/*', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: process.env.PORT || 8081 })
+    await fastify.listen({ port: process.env.PORT || "8081", host: '0.0.0.0' })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
